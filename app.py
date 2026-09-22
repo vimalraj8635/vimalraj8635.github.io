638089395666ef97cd66e3212fab68dc60fadb78
Vimalraj G <123892879+vimalraj8635@users.noreply.github.com>
2026-09-22 13:02:36 +0530

    Configure MAIL_USE_SSL in app.py
    Added configuration for SSL usage in mail settings.

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ CORS(app)
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 
